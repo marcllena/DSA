cc8cee1654996f8525b73f4c47b11e06c7221a7b
gabrigoca <36449282+gabrigoca@users.noreply.github.com>
2018-02-15 12:57:51 +0100

    Clase Rectangle actualitzada y clase quadrat creada

--- provaGeometria/src/com/company/Rectangle.java
+++ provaGeometria/src/com/company/Rectangle.java
@@ -7,6 +7,22 @@ public class Rectangle extends Figures
 {
     private double l1;
     private double l2;
+    public void setL1(double l1)
+    {
+        this.l1=l1;
+    }
+    public void setL2(double l2)
+    {
+        this.l2=l2;
+    }
+    public double getL1()
+    {
+        return this.l1;
+    }
+    public double getL2()
+    {
+        return this.l2;
+    }
     public double area()
     {
         double a;
