05ff08d4516c26fa732b1cf3c8a557d4116677be
jordicros <36449492+jordicros@users.noreply.github.com>
2018-02-15 12:55:07 +0100

    Classe figura

--- provaGeometria/src/com/company/Figures.java
+++ provaGeometria/src/com/company/Figures.java
@@ -3,11 +3,9 @@ package com.company;
 /**
  * Created by jordi on 14/02/2018.
  */
-public abstract class Figures {
-    public void area()
-    {
-        //Prueba
-    }
+abstract public class Figures {
+
+    abstract public double area();
     public double compareTo(Figures in)
     {
         return this.area()-in.area();
