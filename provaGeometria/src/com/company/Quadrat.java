1532abe3a046dd090f4d642426bc3f7967aa0917
gabrigoca <36449282+gabrigoca@users.noreply.github.com>
2018-02-15 13:05:41 +0100

    canvis a quadrat

--- provaGeometria/src/com/company/Quadrat.java
+++ provaGeometria/src/com/company/Quadrat.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Quadrat extends Rectangle
 {
-    public void Quadrat(double l)
+    public Quadrat(double l)
     {
         super.setL1(l);
         super.setL2(l);
