0d0d8f5af3d565bf5b793c6cb608f833722a1e9b
marcllena <marcllena@hotmail.com>
2018-02-15 12:54:51 +0100

    Afegir triangle i arreglar area

--- provaGeometria/src/com/company/Figures.java
+++ provaGeometria/src/com/company/Figures.java
@@ -4,10 +4,7 @@ package com.company;
  * Created by jordi on 14/02/2018.
  */
 public abstract class Figures {
-    public void area()
-    {
-        //Prueba
-    }
+    abstract public double area();
     public double compareTo(Figures in)
     {
         return this.area()-in.area();
---
+++ provaGeometria/src/com/company/Triangle.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by jordi on 14/02/2018.
+ */
+public class Triangle extends Figures
+{
+    private double a;
+    private double h;
+    public double area()
+    {
+        double a=0;
+        a = (a*h)/2;
+        return a;
+    }
+}
