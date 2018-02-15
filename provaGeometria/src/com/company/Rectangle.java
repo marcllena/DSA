3bb7cd35cc0608245c8db50b6bdafdbdd1a07236
gabrigoca <36449282+gabrigoca@users.noreply.github.com>
2018-02-15 13:16:47 +0100

    Constructor quadrat done

--- provaGeometria/src/com/company/Rectangle.java
+++ provaGeometria/src/com/company/Rectangle.java
@@ -7,6 +7,11 @@ public class Rectangle extends Figures
 {
     private double l1;
     private double l2;
+    public Rectangle(double l1,double l2)
+    {
+        this.setL1(l1);
+        this.setL2(l2);
+    }
     public void setL1(double l1)
     {
         this.l1=l1;
