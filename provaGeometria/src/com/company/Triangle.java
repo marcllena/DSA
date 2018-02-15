ffebebba74bc51bce4bb24f03eff1ba394d2ceca
marcllena <marcllena@hotmail.com>
2018-02-15 13:09:40 +0100

    Constructor al triangle

--- provaGeometria/src/com/company/Triangle.java
+++ provaGeometria/src/com/company/Triangle.java
@@ -7,6 +7,24 @@ public class Triangle extends Figures
 {
     private double a;
     private double h;
+
+    public void setA(double l1)
+    {
+        this.a=a;
+    }
+    public void setH(double l2)
+    {
+        this.h=h;
+    }
+    public double getA()
+    {
+        return this.a;
+    }
+    public double getH()
+    {
+        return this.h;
+    }
+
     public double area()
     {
         double a=0;
